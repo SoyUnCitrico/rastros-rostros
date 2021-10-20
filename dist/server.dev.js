524b76aa79f32743f0ca8251b2ef9f59b0d8f7b2
SoyUnCitrico <1darenastolteca@gmail.com>
2021-10-20 13:32:45 -0500

    Habilitando streaming

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -5,15 +5,12 @@ var express = require('express');
 var db = require('./db.js');
 
 var router = require('./network/routes'); // Descomentar para probar en local
+// const {DB_URI, PORT} = require('./config.js');
+// Descomentar para probar en el servidor heroku
 
 
-var _require = require('./config.js'),
-    DB_URI = _require.DB_URI,
-    PORT = _require.PORT; // Descomentar para probar en el servidor heroku
-// const DB_URI = process.env.DB_URI;
-// const PORT = process.env.PORT;
-
-
+var DB_URI = process.env.DB_URI;
+var PORT = process.env.PORT;
 db.connect(DB_URI);
 var app = express();
 app.use(express.json({
